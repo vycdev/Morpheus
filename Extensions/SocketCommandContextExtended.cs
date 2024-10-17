fae885856a9d39c72854840a4298ef361da4f241
Victor-Iulian Sandu <vyctor661@gmail.com>
2024-10-17 19:16:46 +0300

    custom socket command context

---
+++ Extensions/SocketCommandContextExtended.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Morpheus.Database.Models;
+
+namespace Morpheus.Extensions;
+public class SocketCommandContextExtended(
+    DiscordSocketClient client, 
+    SocketUserMessage msg, 
+    Guild? guild, 
+    User? user
+) : SocketCommandContext(client, msg)
+{
+    public Guild? DbGuild { get; set; } = guild;
+    public User? DbUser { get; set; } = user;
+}
