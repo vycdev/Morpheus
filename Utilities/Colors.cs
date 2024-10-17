795cfcbab293123b97e4d63ea232a7f76e109d31
Victor-Iulian Sandu <vyctor661@gmail.com>
2024-10-17 20:25:21 +0300

    predefined colors

---
+++ Utilities/Colors.cs
@@ -0,0 +1,11 @@
+﻿
+using Discord;
+
+namespace Morpheus.Utilities;
+public static class Colors
+{
+    public static Color Blue = new(50, 153, 254);
+    public static Color BlueShadow = new(19, 61, 101);
+    public static Color White = new(255, 255, 255);
+    public static Color WhiteShadow = new(185, 229, 254);
+}
