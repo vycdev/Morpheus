56eada50bcd5f23e55d53b80e47b7bc36d03c444
Victor Sandu <vyctor661@gmail.com>
2025-05-05 18:49:13 +0300

    update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Resources;
 [assembly: AssemblyDescription("A discord bot with generic functionalities, also sentient.")]
 
 // Version information
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.2.*")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
