264baf462aac054816018369151adb6a1dfa7393
Victor Sandu <vyctor661@gmail.com>
2025-06-18 23:09:33 +0300

    feat: levels command module
    2 commands, a leaderboard and a current level command

---
+++ Modules/LevelsModule.cs
@@ -0,0 +1,104 @@
+﻿using Discord.Commands;
+using Morpheus.Attributes;
+using Morpheus.Database;
+using Morpheus.Database.Models;
+using Morpheus.Extensions;
+using Morpheus.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morpheus.Modules;
+
+public class LevelsModule(DB dbContext) : ModuleBase<SocketCommandContextExtended>
+{
+    [Name("Current Level")]
+    [Summary("Displays the current level and experience points of the user.")]
+    [Command("level")]
+    [Alias("lvl", "currentlevel", "currentxp")]
+    [RateLimit(3, 10)]
+    public async Task CurrentLevelAsync()
+    {
+        var user = Context.DbUser;
+        var guild = Context.DbGuild;
+
+        if (user == null || guild == null)
+        {
+            await ReplyAsync("User or guild not found.");
+            return;
+        }
+
+        IQueryable<UserLevels> userLevels = dbContext.UserLevels
+            .Where(ul => ul.UserId == user.Id);
+
+        UserLevels? userLevelGuild = userLevels
+            .FirstOrDefault(ul => ul.GuildId == guild.Id);
+
+        if(userLevels.Count() == 0)
+        { 
+            await ReplyAsync("There is no level information available for you in any guild.");
+            return;
+        }
+
+        long totalXp = userLevels.Sum(ul => ul.TotalXp);
+        int totalLevel = ActivityHandler.CalculateLevel(totalXp);
+        long totalXpNeededForNextLevel = ActivityHandler.CalculateXp(totalLevel + 1);
+
+        await ReplyAsync($"**Global**: Level **{totalLevel}** with **{totalXp}** XP");
+        await ReplyAsync($"**{totalXpNeededForNextLevel - totalXp}** XP needed to level up globally \n");
+
+        if (userLevelGuild != null)
+        {
+            await ReplyAsync($"**{guild.Name}**: Level **{userLevelGuild.Level}** with **{userLevelGuild.TotalXp}** XP");
+
+            totalXpNeededForNextLevel = ActivityHandler.CalculateXp(userLevelGuild.Level + 1);
+
+            await ReplyAsync($"**{totalXpNeededForNextLevel - userLevelGuild.TotalXp}** XP needed to level up");
+            return;
+        }
+    }
+
+    [Name("Leaderboard")]
+    [Summary("Displays the leaderboard of users in the guild based on their levels.")]
+    [Command("leaderboard")]
+    [Alias("lb", "top", "topusers")]
+    [RateLimit(3, 10)]
+    public async Task LeaderboardAsync(int page = 1)
+    {
+        var guild = Context.DbGuild;
+        if (guild == null)
+        {
+            await ReplyAsync("Guild not found.");
+            return;
+        }
+
+        IQueryable<UserLevels> userLevels = dbContext.UserLevels
+            .Where(ul => ul.GuildId == guild.Id)
+            .OrderByDescending(ul => ul.TotalXp)
+            .Take(50);
+
+        int totalUsers = userLevels.Count();
+        int totalPages = (int)Math.Ceiling(totalUsers / (double)10);
+        
+        if (page < 1 || page > totalPages)
+        {
+            await ReplyAsync($"Invalid page number. Please choose a page between 1 and {totalPages}.");
+            return;
+        }
+
+        var leaderboard = userLevels
+            .Skip((page - 1) * 10)
+            .Take(10)
+            .Select(ul => $"**{ul.User.Username}**: Level **{ul.Level}** with **{ul.TotalXp}** XP")
+            .ToList();
+
+        StringBuilder sb = new();
+        
+        sb.AppendLine($"**Leaderboard for {guild.Name}** (Page {page}/{totalPages})");
+        sb.AppendLine(string.Join("\n", leaderboard));
+
+        await ReplyAsync(sb.ToString());
+    }
+}
