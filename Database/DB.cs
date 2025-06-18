70f3296a6b7cdb2a910828660feb1c079ee562aa
Victor Sandu <vyctor661@gmail.com>
2025-06-18 19:13:17 +0300

    feat: added table UserLevels to database
    generated migrations

--- Database/DB.cs
+++ Database/DB.cs
@@ -28,6 +28,7 @@ public class DB : Microsoft.EntityFrameworkCore.DbContext
 
     public DbSet<Models.User> Users { get; set; }
     public DbSet<Models.UserActivity> UserActivity { get; set; }
+    public DbSet<Models.UserLevels> UserLevels { get; set; }
     public DbSet<Models.Guild> Guilds { get; set; }
     public DbSet<Models.Quote> Quotes { get; set; }
     public DbSet<Models.Log> Logs { get; set; }
