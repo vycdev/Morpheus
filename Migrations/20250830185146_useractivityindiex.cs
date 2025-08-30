efaef7f15c0c570403eb07b491782aaaea2128eb
Victor Sandu <vyctor661@gmail.com>
2025-08-30 21:52:00 +0300

    feat: update UserActivity index to include GuildId and InsertDate for improved query performance

---
+++ Migrations/20250830185146_useractivityindiex.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Morpheus.Migrations
+{
+    /// <inheritdoc />
+    public partial class useractivityindiex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserActivity_UserId",
+                table: "UserActivity");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserActivity_UserId_GuildId_InsertDate",
+                table: "UserActivity",
+                columns: new[] { "UserId", "GuildId", "InsertDate" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserActivity_UserId_GuildId_InsertDate",
+                table: "UserActivity");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserActivity_UserId",
+                table: "UserActivity",
+                column: "UserId");
+        }
+    }
+}
