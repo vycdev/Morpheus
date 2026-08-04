ca6c7bb6d034b930363c67dcdff9cc8ad81cb574
vycdev2 <vycdev2@users.noreply.github.com>
2026-08-04 05:32:36 +0000

    fix: detect punctuated IPv4 URLs

--- Morpheus.Tests/UtilsTests.cs
+++ Morpheus.Tests/UtilsTests.cs
@@ -25,4 +25,30 @@ public class UtilsTests
     {
         Assert.False(Utils.ContainsUrl(text));
     }
+
+    [Theory]
+    [InlineData("(192.168.1.1)")]
+    [InlineData("\"192.168.1.1\"")]
+    [InlineData("See:192.168.1.1")]
+    [InlineData("Visit 192.168.1.1.")]
+    [InlineData("Connect to 192.168.1.1:8080/path")]
+    public void ContainsUrl_DetectsIpv4AddressesAfterPunctuation(string text)
+    {
+        Assert.True(Utils.ContainsUrl(text));
+    }
+
+    [Theory]
+    [InlineData("prefix192.168.1.1")]
+    [InlineData("user@192.168.1.1")]
+    [InlineData("user@[192.168.1.1]")]
+    [InlineData("user@[IPv4:192.168.1.1]")]
+    [InlineData("192.168.1.1.2")]
+    [InlineData("192.168.1.1suffix")]
+    [InlineData("192.168.1.1:abc")]
+    [InlineData("192.168.1.1:123456")]
+    [InlineData("192.168.1.1:80suffix")]
+    public void ContainsUrl_DoesNotTreatEmbeddedIpv4AddressesAsUrls(string text)
+    {
+        Assert.False(Utils.ContainsUrl(text));
+    }
 }
