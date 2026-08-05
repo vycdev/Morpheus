23201125341f34e733f66e7e5f30c48a48e7ea99
vycdev2 <vycdev2@users.noreply.github.com>
2026-08-05 05:14:01 +0000

    chore: remove dashboard end to end

--- Program.cs
+++ Program.cs
@@ -1,30 +1,20 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Morpheus.Dashboard;
 using Morpheus.Extensions;
 using Morpheus.Utilities;
 
 Env.Load(".env");
 
-DashboardApiOptions dashboardOptions = DashboardApiOptions.FromEnvironment();
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services
+            .AddBotServices()
+            .AddBotJobs()
+            .AddBotHandlers()
+            .AddBotDatabase();
+    })
+    .Build();
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls(dashboardOptions.Urls);
-
-builder.Services
-    .AddBotServices()
-    .AddBotJobs()
-    .AddBotHandlers()
-    .AddBotDatabase()
-    .AddDashboardApi(dashboardOptions);
-
-WebApplication app = builder.Build();
-
-app.UseCors();
-app.UseOutputCache();
-app.MapDashboardApi();
-
-app.RunStartupMigrations();
-await app.StartBotAsync();
-await app.RunAsync();
+host.RunStartupMigrations();
+await host.StartBotAsync();
+await host.RunAsync();
